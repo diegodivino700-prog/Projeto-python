bc8e4cca3f71de4f55789a3632ba3d1ccdcadd5e
diegodivino700-prog <diegodivino700@gmail.com>
2026-05-04 14:17:55 +0000

    v1

---
+++ quizz.py
@@ -0,0 +1,9 @@
+print("Bem vindo quiz futebolistico")
+
+questao1 = input("1-Qual jogador ganhou a bola de ouro em 2022?\ a-Lionel Messi \n b-Cristiano Ronaldo \n c-Robert Lewandowski").upper()
+if questao1 == "A":
+  print("Resposta Correta👍")
+else:
+  print("Resposta incorreta! a resposta correta é A-Lionel Messi.")
+
+  
