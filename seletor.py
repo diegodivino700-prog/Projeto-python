bc4b190a5927f291983f958ee1450cb9e1cea0b8
diegodivino700-prog <diegodivino700@gmail.com>
2026-05-04 10:06:08 -0300

    V1

---
+++ seletor.py
@@ -0,0 +1,9 @@
+#--seletor de caracteres--
+letra = input("Digite a Letra")
+if letra == "A":
+  print("Você escolheu a Letra A")
+elif letra == "B":
+   print("Você escolheu a Letra B")
+else:
+   print("Essa opção não está Disponível")
+  
