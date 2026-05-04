9d82ca1adaeb93fd140531bfd71e85daa321bc33
diegodivino700-prog <diegodivino700@gmail.com>
2026-05-04 13:04:05 -0300

    quizz.py v2

---
+++ quizz.py
@@ -0,0 +1,14 @@
+print("Bem vindo ao quiz Futebolisco")
+
+questao1 = input("1-Qual jogador ganhou a bola de ouro em 2022?\ a-Lionel Messi \n b-Cristiano Ronaldo \n c-Robert Lewandowski").upper()
+if questao1 == "A":
+  print("Resposta Correta👍")
+else:
+  print("Resposta Incorreta! a resposta correta é A-Lionel Messi.")
+  
+questao2 = input("2-Qual é a competição de seleções mais importante do mundo?\ a-Copa Ámerica \n b-Copa do Mundo \n c-Eurocopa").upper()
+if questao2 == "B":
+  print("Resposta Correta👍")
+else:
+  print("Resposta Incorreta! a resposta correta é B-Copa do Mundo.")
+  
